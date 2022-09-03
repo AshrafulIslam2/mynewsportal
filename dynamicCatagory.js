454a36f4d4faed3cacf2cd74d16c686e2fbf30a3
Ashraful Islam <ashrafulislam21211@gmail.com>
2022-09-03 20:30:39 +0600

    Q/A done

--- dynamicCatagory.js
+++ dynamicCatagory.js
@@ -31,22 +31,22 @@ const loadNews = news => {
         newsDiv.classList.add('g-1');
         newsDiv.innerHTML = `
         <div class=" col-12 col-md-3 m-0 p-0 text-center text-sm-start text-md-start text-lg ">
-            <img src="${element.thumbnail_url}" class="img-fluid p-3 rounded-start" alt="...">
+            <img src="${element.thumbnail_url ?element.thumbnail_url : "No Thumblin found"}" class="img-fluid p-3 rounded-start" alt="...">
         </div>
         <div class="col-12 col-md-8">
             <div class="card-body">
-                <h5 class="card-title">${element.title}</h5>
-                <p class="card-text">${element.details}</p>
+                <h5 class="card-title">${element.title ?element.title :"no title found" }</h5>
+                <p class="card-text">${element.details ?element.details :"no details found"}</p>
                 <div class="d-lg-flex justify-content-between align-items-center">
                    <div class="d-flex mb-4">
-                   <img class="me-2 rounded-circle" src="${element.author.img}" width=45px alt="" srcset="">
+                   <img class="me-2 rounded-circle" src="${element.author.img ? element.author.img :"No author image found"}" width=45px alt="" srcset="">
                     <div>
-                        <p class="card-text m-0"><small class="text-muted">${element.author.name}</small></p>
-                        <p class="card-text m-0 p-0"><small class="text-muted">${element.author.published_date}</small></p>
+                        <p class="card-text m-0"><small class="text-muted">${element.author.name ?element.author.name  :"NO name found"}</small></p>
+                        <p class="card-text m-0 p-0"><small class="text-muted">${element.author.published_date?element.author.published_date:"no publish days found"}</small></p>
                     </div>
                    </div>
                     <div>
-                        <i class="fa-regular fa-eye"> <span>${element.total_view}</span> M</i>
+                        <i class="fa-regular fa-eye"> <span>${element.total_view?element.total_view :"0" }</span> M</i>
                     </div>
                     <div>
                         <i class="fa-regular fa-star"></i>
@@ -89,19 +89,19 @@ const displayNewsDetails = data => {
                         </div>
                         <div class="">
                             <div class="card modal-body">
-                                <img src="${data.image_url}" class="card-img-top" alt="...">
+                                <img src="${data.image_url? data.image_url: "no Image found"}" class="card-img-top" alt="...">
                                 <div class="card-body">
-                                    <h5 class="card-title">${data.title}</h5>
+                                    <h5 class="card-title">${data.title?data.title:"No title found"}</h5>
                                     <p class="card-text">Some quick example text to build on the card title and make up
                                         the bulk of the card's content.</p>
                                         <div class="d-flex mb-4">
-                                        <img class="me-2 rounded-circle" src="${data.author.img}" width=45px alt="" srcset="">
+                                        <img class="me-2 rounded-circle" src="${data.author.img ?data.author.img :"no author image found"}" width=45px alt="" srcset="">
                                          <div>
-                                             <p class="card-text m-0"><small class="text-muted">${data.author.name}</small></p>
+                                             <p class="card-text m-0"><small class="text-muted">${data.author.name?data.author.name:"no author name"}</small></p>
                                              <p class="card-text m-0 p-0"><small class="text-muted">${data.author.published_date}</small></p>
                                          </div>
                                          <div>
-                                         <i class="fa-regular fa-eye"> <span>${data.total_view}</span> M</i>
+                                         <i class="fa-regular fa-eye"> <span>${data.total_view ? data.total_view:"0"}</span> M</i>
                                          </div>
                                         </div>
                                        
--- loadpage.js
+++ loadpage.js
@@ -5,11 +5,11 @@ document.getElementById("newsbar").addEventListener('click', function () {
     window.location.href='index.html';
 })
 
-function loadAllNews() {
-    fetch(`https://openapi.programming-hero.com/api/news/category/08`)
-    .then((response) => response.json())
-     .then((data) => home(data.data))
-}
-const home = allnews => {
-    console.log(allnews);
- }
+// function loadAllNews() {
+//     fetch(`https://openapi.programming-hero.com/api/news/category/08`)
+//     .then((response) => response.json())
+//      .then((data) => home(data.data))
+// }
+// const home = allnews => {
+//     console.log(allnews);
+//  }
