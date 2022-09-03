49238884b96a8f9c9a53889acb0722c656246f44
Ashraful Islam <ashrafulislam21211@gmail.com>
2022-09-03 18:28:22 +0600

    dynamic Page load done

---
+++ loadpage.js
@@ -0,0 +1,6 @@
+document.getElementById("blogs").addEventListener('click', function () {
+    window.location.href='blog.html';
+})
+document.getElementById("newsbar").addEventListener('click', function () {
+    window.location.href='index.html';
+})
