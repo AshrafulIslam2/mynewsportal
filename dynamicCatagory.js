862f49a0184a83df6f4fd632b289f342060f0f3a
Ashraful Islam <ashrafulislam21211@gmail.com>
2022-09-03 12:05:52 +0600

    total news found section done

--- dynamicCatagory.js
+++ dynamicCatagory.js
@@ -3,14 +3,16 @@ fetch('https://openapi.programming-hero.com/api/news/categories')
   .then((data) => loadAllCatagory(data.data.news_category));
 
 
-function loadAllCatagory(catagoris){
+function loadAllCatagory(catagoris) {
+    console.log(catagoris);
     catagoris.forEach(element => {
         const catagoryUl = document.getElementById('dynamicCatagory');
         const list = document.createElement('li');
         list.classList.add('nav-item');
         list.innerHTML = `
-        <a class="nav-link active text-white" aria-current="page" href="#">${element.category_name}</a>
+        <a class="nav-link active text-white" aria-current="page" href="#"  onclick="loadNews(${element.category_id})">${element.category_name}</a>
         `
         catagoryUl.appendChild(list);
+         console.log(element.category_id)
     });
   }
